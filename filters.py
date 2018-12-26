34f5f30a1f7feb2aa7a5a3a595004360c87bbd79
Lucian Marin <marin.lucian@gmail.com>
2018-12-26 15:34:45 +0200

    fixed shortdate

--- filters.py
+++ filters.py
@@ -8,27 +8,24 @@ def hostname(value):
     return url.netloc.replace('www.', '')
 
 
-def date(struct):
+def date(stamp):
     """Format date time."""
-    timestamp = datetime.fromtimestamp(struct)
+    timestamp = datetime.fromtimestamp(stamp)
     return timestamp.strftime('%b %-e, %Y %H:%M')
 
 
-def shortdate(struct):
+def shortdate(stamp):
     """Short time interval for a timestamp."""
-    timestamp = datetime.utcfromtimestamp(struct)
+    timestamp = datetime.utcfromtimestamp(stamp)
     delta = datetime.utcnow() - timestamp
-
-    miliseconds = abs(delta.microseconds) // 1000
-    seconds = abs(delta.seconds)
+    miliseconds = int(delta.microseconds) // 1000
+    seconds = int(delta.total_seconds())
     minutes = seconds // 60
     hours = seconds // 3600
-
     if not hours and not minutes and not seconds:
-        return "%dms" % miliseconds
-    elif not hours and not minutes:
-        return "%ds" % seconds
-    elif not hours:
-        return "%dm" % minutes
-    else:
-        return "%dh" % hours
+        return f"{miliseconds}ms"
+    if not hours and not minutes:
+        return f"{seconds}s"
+    if not hours:
+        return f"{minutes}m"
+    return f"{hours}h"
