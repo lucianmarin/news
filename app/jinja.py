ddd214af7090ced80c59fa7b87a9b2b66caf83d1
Lucian Marin <marin.lucian@gmail.com>
2019-01-09 11:56:41 +0200

    update header again

--- app/jinja.py
+++ app/jinja.py
@@ -13,5 +13,5 @@ def environment(**options):
     env.filters['superscript'] = superscript
     # globals
     env.globals['reverse'] = reverse
-    env.globals['v'] = 4
+    env.globals['v'] = 5
     return env
