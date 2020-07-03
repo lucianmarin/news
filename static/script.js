81af9d49f74dca2aba3b8cc7e5f33153188f0eb6
Lucian Marin <marin.lucian@gmail.com>
2020-07-03 20:17:20 +0300

    highlight while reading

--- static/script.js
+++ static/script.js
@@ -1,31 +1,42 @@
-function listen(event) {
-    event.preventDefault();
-    var element = event.currentTarget;
-    var state = element.innerText;
-
-    var synth = speechSynthesis;
-    var article = document.getElementById('article');
+function utterThis(synth, p) {
+    var initial = p.innerText;
     var utterance = new SpeechSynthesisUtterance();
-    utterance.text = article.textContent;
+    utterance.text = p.innerText;
     utterance.pitch = 1;
     utterance.rate = 1.25;
     utterance.onend = function() {
-        element.innerText = "listen";
+        p.innerHTML = initial;
     };
+    utterance.onboundary = function(e) {
+        before = initial.slice(0, e.charIndex);
+        inner = initial.slice(e.charIndex, e.charIndex + e.charLength);
+        after = initial.slice(e.charIndex + e.charLength);
+        p.innerHTML = before + '<u>' + inner + '</u>' + after;
+    }
+    synth.speak(utterance);
+}
 
+function listen(event) {
+    event.preventDefault();
+    var button = event.currentTarget;
+    var state = button.innerText;
+    var synth = speechSynthesis;
+    var article = document.getElementById('article');
     if (state == "listen") {
         synth.cancel();
-        synth.speak(utterance);
-        element.innerText = "pause";
+        button.innerText = "pause";
+        article.childNodes.forEach(p => {
+            if (p.innerText && !p.children.length) {
+                utterThis(synth, p);
+            }
+        })
     }
-
     if (state == "pause") {
         synth.pause();
-        element.innerText = "resume";
+        button.innerText = "resume";
     }
-
     if (state == "resume") {
         synth.resume();
-        element.innerText = "pause";
+        button.innerText = "pause";
     }
 }
