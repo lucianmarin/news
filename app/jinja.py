fdf0b2ee0038fb23c186b08aeb6433c72c74bb49
Lucian Marin <marin.lucian@gmail.com>
2021-12-04 21:33:11 +0200

    hostname reads

--- app/jinja.py
+++ app/jinja.py
@@ -14,4 +14,4 @@ env.filters['superscript'] = superscript
 env.filters['truncate'] = truncate
 
 env.globals['brand'] = "Unfeeder"
-env.globals['v'] = 47
+env.globals['v'] = 48
--- static/script.js
+++ static/script.js
@@ -4,17 +4,17 @@ function utterThis(synth, p) {
     utterance.text = p.innerText;
     utterance.pitch = 1;
     utterance.rate = 1.25;
-    utterance.onend = function() {
+    utterance.onend = function () {
         p.innerHTML = initial;
     };
-    utterance.onboundary = function(e) {
+    utterance.onboundary = function (e) {
         if (e.charLength) {
             before = initial.slice(0, e.charIndex);
             inner = initial.slice(e.charIndex, e.charIndex + e.charLength);
             after = initial.slice(e.charIndex + e.charLength);
-            p.innerHTML = before + '<u>' + inner + '</u>' + after;
+            p.innerHTML = before + "<u>" + inner + "</u>" + after;
         }
-    }
+    };
     synth.speak(utterance);
 }
 
@@ -23,15 +23,15 @@ function listen(event) {
     var button = event.currentTarget;
     var state = button.innerText;
     var synth = speechSynthesis;
-    var article = document.getElementById('article');
+    var article = document.getElementById("article");
     if (state == "listen") {
         synth.cancel();
         button.innerText = "pause";
-        article.childNodes.forEach(p => {
+        article.childNodes.forEach((p) => {
             if (p.innerText && !p.children.length) {
                 utterThis(synth, p);
             }
-        })
+        });
     }
     if (state == "pause") {
         synth.pause();
